e2859098e43c4cbd4f72ee01d9ed8bd5e71ea345
Alessandro <20360165+kothonet@users.noreply.github.com>
2018-08-30 12:25:05 -0300

    e691c72

---
+++ precache-manifest.de6e00f781cf7944d9ec210c4b692b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e69f8d287bcd12f87ae7",
+    "url": "/js/chunk-vendors.da5fe936.js"
+  },
+  {
+    "revision": "69bc92e1e595828f7801",
+    "url": "/js/app.a272e30e.js"
+  },
+  {
+    "revision": "a5bab0c27f9145c1bdd284ea9c8a734f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69f8d287bcd12f87ae7",
+    "url": "/css/chunk-vendors.bd48805f.css"
+  },
+  {
+    "revision": "69bc92e1e595828f7801",
+    "url": "/css/app.89e603fa.css"
+  }
+];
