05a45167161084dbd2b863db6195677ee6b4139a
Alessandro <20360165+kothonet@users.noreply.github.com>
2018-08-31 11:32:55 -0300

    implementação das ferramentas

--- src/components/Ferramenta.vue
+++ src/components/Ferramenta.vue
@@ -6,7 +6,7 @@
               <v-toolbar-title>Ferramentas</v-toolbar-title>
             </v-toolbar>
             <v-list two-line>
-              <div v-for="ferramenta in ferramentas" :key="ferramenta.id">
+              <div v-for="ferramenta in availableFerramentas" :key="ferramenta.id">
               <v-list-tile @click="selectferramenta(ferramenta.id)" >
                 <v-list-tile-content>
                   <v-list-tile-title v-text="ferramenta.nome"></v-list-tile-title>
@@ -28,7 +28,88 @@ export default {
   data() {
     return {
       enfase: null,
-      ferramentas: []
+      ferramentas: [
+        {
+          id: 1,
+          nome: 'CACOO',
+          selecao: [
+            {enfase:2,criterio:1,formato:1},
+            {enfase:2,criterio:3,formato:1}
+          ]
+        },
+        {
+          id: 2,
+          nome: 'CANVA',
+          selecao: [
+            {enfase:1,criterio:1,formato:1},
+            {enfase:1,criterio:2,formato:1},
+            {enfase:1,criterio:3,formato:1},
+            {enfase:1,criterio:4,formato:1},
+            {enfase:4,criterio:1,formato:1},
+            {enfase:4,criterio:2,formato:1}
+          ]
+        },
+        {
+          id: 3,
+          nome: 'CREATELY',
+          selecao: [
+            {enfase:2,criterio:2,formato:1},
+            {enfase:2,criterio:4,formato:1}
+          ]
+        },
+        {
+          id: 4,
+          nome: 'EASELY',
+          selecao: []
+        },
+        {
+          id: 5,
+          nome: 'INFOGRAM',
+          selecao: [
+            {enfase:2,criterio:1,formato:2},
+            {enfase:2,criterio:2,formato:2},
+            {enfase:2,criterio:3,formato:2},
+            {enfase:2,criterio:4,formato:2}
+          ]
+        },
+        {
+          id: 6,
+          nome: 'PIKTOCHART',
+          selecao: [
+            {enfase:3,criterio:2,formato:1},
+            {enfase:3,criterio:3,formato:1}
+          ]
+        },
+        {
+          id: 7,
+          nome: 'VENNGAGE',
+          selecao: [
+            {enfase:4,criterio:2,formato:1}
+          ]
+        },
+        {
+          id: 8,
+          nome: 'VISME',
+          selecao: [
+            {enfase:1,criterio:1,formato:2},
+            {enfase:1,criterio:2,formato:2},
+            {enfase:1,criterio:3,formato:2},
+            {enfase:1,criterio:4,formato:2},
+            {enfase:3,criterio:1,formato:1},
+            {enfase:3,criterio:1,formato:2},
+            {enfase:3,criterio:2,formato:1},
+            {enfase:3,criterio:2,formato:2},
+            {enfase:3,criterio:3,formato:1},
+            {enfase:3,criterio:3,formato:2},
+            {enfase:3,criterio:4,formato:1},
+            {enfase:3,criterio:4,formato:2},
+            {enfase:4,criterio:1,formato:2},
+            {enfase:4,criterio:2,formato:1},
+            {enfase:4,criterio:2,formato:2}
+          ]
+        }
+      ],
+      availableFerramentas: []
     };
   },
   methods: {
